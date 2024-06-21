602f6edec9109f14cdf7844959aee1e3227541e2
Tal Kohavy <talkohavy@gmail.com>
2024-06-21 18:09:38 +0300

    added a session manager

---
+++ src/SessionManager/SessionManager.js
@@ -0,0 +1,48 @@
+import { v4 as uuid } from 'uuid';
+import { SESSION_KEYS } from './constants';
+
+/**
+ * @type {SessionManager}
+ */
+let sessionManager = null;
+
+class SessionManager {
+  constructor() {
+    const localStorageId = localStorage.getItem(SESSION_KEYS.LOCAL_STORAGE);
+    const sessionStorageId = sessionStorage.getItem(SESSION_KEYS.SESSION_STORAGE);
+
+    if (!localStorageId) {
+      localStorage.setItem(SESSION_KEYS.LOCAL_STORAGE, uuid());
+    }
+
+    if (!sessionStorageId) {
+      sessionStorage.setItem(SESSION_KEYS.SESSION_STORAGE, uuid());
+    }
+  }
+
+  setSessionId() {
+    const localStorageId = localStorage.getItem(SESSION_KEYS.LOCAL_STORAGE);
+    const sessionStorageId = sessionStorage.getItem(SESSION_KEYS.SESSION_STORAGE);
+
+    if (!localStorageId) {
+      localStorage.setItem(SESSION_KEYS.LOCAL_STORAGE, uuid());
+    }
+
+    if (!sessionStorageId) {
+      sessionStorage.setItem(SESSION_KEYS.SESSION_STORAGE, uuid());
+    }
+  }
+
+  getSessionId() {
+    const localStorageId = localStorage.getItem(SESSION_KEYS.LOCAL_STORAGE);
+    const sessionStorageId = sessionStorage.getItem(SESSION_KEYS.SESSION_STORAGE);
+
+    return { localStorageId, sessionStorageId };
+  }
+}
+
+function initSessionManager() {
+  sessionManager = new SessionManager();
+}
+
+export { initSessionManager, sessionManager };

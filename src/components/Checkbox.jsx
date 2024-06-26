cb51314b595a4096f3ce3ca466af9c33255ef332
Tal Kohavy <talkohavy@gmail.com>
2024-06-26 11:35:34 +0300

    Checkbox component is fixed

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+
 export default function Checkbox({ isChecked, setIsChecked, label }) {
   return (
     <label className='group flex cursor-pointer items-center justify-center gap-2'>
@@ -6,8 +8,9 @@ export default function Checkbox({ isChecked, setIsChecked, label }) {
           type='checkbox'
           // value={isChecked} // <--- apparently you don't need value in a checkbox
           checked={!!isChecked}
-          onChange={setIsChecked}
-          className='size-0 opacity-0'
+          onChange={() => setIsChecked(!isChecked)}
+          // eslint-disable-next-line
+          className={clsx(isChecked && 'checked', 'size-0 opacity-0')}
         />
 
         <div className='size-6 rounded-md border border-black group-has-[input:checked]:bg-blue-400' />
